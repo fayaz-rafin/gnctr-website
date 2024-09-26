59c4b19fdbb3eaaff3ba64b3e62f36b5a7a35021
fayaz-rafin <fayaz.rafin@gmail.com>
2024-09-26 17:15:52 -0400

    updated homepage

--- blog/app/page.tsx
+++ blog/app/page.tsx
@@ -3,15 +3,14 @@ import { BlogPosts } from 'app/components/posts'
 export default function Page() {
   return (
     <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+      <h1 className="mb-8 text-center text-2xl font-semibold tracking-tighter">
         York University's GNCTR Team
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`York University's Great Northern Concrete Toboggan Race (GNCTR) team is a student-run organization that participates in the annual GNCTR competition, the longest-running engineering competition in Canada. The competition challenges teams to design, construct, and race a toboggan with a running surface made entirely of concrete.`}
+      </p>
+      <p className="mb-4">
+        {`York's team is composed of passionate engineering students from various disciplines, including civil, mechanical, and electrical engineering, working together to push the boundaries of design, creativity, and sustainability. The team focuses on developing innovative solutions in concrete mix design, steering, and braking systems, and integrating cutting-edge technology into their designs.`}
       </p>
       <div className="my-8">
         
